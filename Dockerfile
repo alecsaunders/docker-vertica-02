b34515b453dc1dcd6c01055a7151e8d609a507a4
Alec Saunders <alecsaunders90@gmail.com>
2019-03-08 12:11:20 -0700

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+FROM centos:centos7
+
+ARG VERTICA_PACKAGE="vertica.rpm"
+
+ENV LANG en_US.utf8
+ENV TZ UTC
+
+ADD packages/${VERTICA_PACKAGE} /tmp/
+
+RUN yum -q -y update \
+  && yum -q -y install \
+    iproute \
+    openssl \
+    gdb \
+    mcelog \
+    openssh \
+    sysstat \
+    which
+
+RUN yum localinstall -q -y /tmp/${VERTICA_PACKAGE}
+
+RUN /opt/vertica/sbin/install_vertica --license CE --accept-eula --hosts 127.0.0.1 \
+  --dba-user-password-disabled --failure-threshold NONE --no-system-configuration \
+  && /bin/rm -f /tmp/vertica*
+
+ENV PATH="/opt/vertica/bin:/opt/vertica/packages/kafka/bin:${PATH}"
+
+ADD --chown=root:root ./create_db.sh /opt/vertica/bin/
+ENTRYPOINT ["/opt/vertica/bin/create_db.sh"]
+
+EXPOSE 5433
---
+++ create_db.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+set -e
+
+#################################
+# Handle exit                   #
+#################################
+function shut_down() {
+    echo ""
+    echo "Bye!"
+    exit 0
+}
+trap "shut_down" SIGKILL SIGTERM SIGHUP SIGINT
+
+
+#################################
+# Create and Start the Database #
+#################################
+
+# Set Database Name (Default 'docker')
+if [ -z $DBNAME ]; then
+    DBNAME=docker
+fi
+
+# Command to Create and Start the Database
+CREATE_DB="/opt/vertica/bin/admintools -t create_db --skip-fs-checks -s localhost -d $DBNAME "
+
+# Create DB with password if DBPASSWD environment variable is set
+if [[ -n $DBPASSWD ]]; then
+    CREATE_DB="${CREATE_DB} -p ${DBPASSWD}"
+fi
+
+# Execute create_db command
+echo "Creating database"
+su - dbadmin -c "${CREATE_DB}"
+echo ""
+echo "Vertica is now running..."
+
+
+#################################
+# Docker Entrypoint initdb.d    #
+#################################
+if [ -d /docker-entrypoint-initdb.d/ ]; then
+    echo "Running entrypoint scripts ..."
+    for f in $(ls /docker-entrypoint-initdb.d/* | sort); do
+        case "$f" in
+            *.sh)     echo "$0: running $f"; . "$f" ;;
+            *.sql)    echo "$0: running $f"; su - dbadmin -c "/opt/vertica/bin/vsql -d $DBNAME -f $f"; echo ;;
+            *)        echo "$0: ignoring $f" ;;
+        esac
+        echo
+    done
+    echo "All docker-entrypoint-initdb.d scripts executed..."
+fi
+
+
+#################################
+# Keep Container Active    #
+#################################
+while true; do
+  sleep 1
+done
